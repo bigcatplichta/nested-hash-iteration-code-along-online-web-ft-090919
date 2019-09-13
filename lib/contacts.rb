ca1f0383aa97a53d2d620221bd7607c9a6090023
Matt Plichta <bigcatplichta@gmail.com>
2019-09-13 18:47:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -15,5 +15,11 @@ require 'pry'
   # }
   
 def remove_strawberry(contacts)
-
+  contacts.each do |person, details_hash|
+    if person == "Freddy Mercury"
+      details_hash.each do |attributes, info|
+        if attributes == favorite_ice_cream_flavors
+          
+    binding.pry
+  end
 end
